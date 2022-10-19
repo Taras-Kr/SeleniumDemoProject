0a32b05bcc9b2ff0a1d584334d9a29b6f332564e
Taras Krasitskyi <taras.krasitskyi.gitlab@gmail.com>
2022-10-19 14:53:28 +0300

    add allure support

--- src/test/java/com/taraskrasitsky/ebay/ui/TopMenuTest.java
+++ src/test/java/com/taraskrasitsky/ebay/ui/TopMenuTest.java
@@ -3,6 +3,7 @@ package com.taraskrasitsky.ebay.ui;
 import com.taraskrasitsky.ebay.utils.TestRunner;
 import com.taraskrasitskyi.ebay.ui.pages.CategoryPage;
 import com.taraskrasitskyi.ebay.ui.pages.HomePage;
+import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,6 +12,7 @@ import static com.taraskrasitskyi.ebay.ui.locators.HomePageLocators.*;
 public class TopMenuTest extends TestRunner {
 
     @Test(description = "Verify that an unregistered user can open category 'Electronics' from the top menu")
+    @Description ("Verify that an unregistered user can open category 'Electronics' from the top menu")
     public void verifiesThatCategoryOpens() {
         CategoryPage categoryPage = new HomePage(driver).openCategoryFromTopMenu(TOP_MENU_ELECTRONICS.getPath());
         String actualCaption = categoryPage.getCaption();
