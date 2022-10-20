5101e95b25d009c0dfa41e459a936efc1955d1d0
Taras Krasitskyi <taras.krasitskyi.gitlab@gmail.com>
2022-10-20 16:10:15 +0300

    implement openCSV

--- src/test/java/com/taraskrasitsky/ebay/ui/TopMenuTest.java
+++ src/test/java/com/taraskrasitsky/ebay/ui/TopMenuTest.java
@@ -12,12 +12,13 @@ import static com.taraskrasitskyi.ebay.ui.locators.HomePageLocators.*;
 public class TopMenuTest extends TestRunner {
 
     @Test(description = "Verify that an unregistered user can open category 'Electronics' from the top menu")
-    @Description ("Verify that an unregistered user can open category 'Electronics' from the top menu")
+    @Description("Verify that an unregistered user can open category 'Electronics' from the top menu")
     public void verifiesThatCategoryOpens() {
         CategoryPage categoryPage = new HomePage(driver).openCategoryFromTopMenu(TOP_MENU_ELECTRONICS.getPath());
         String actualCaption = categoryPage.getCaption();
         Assert.assertEquals(actualCaption, "Electronics");
-        String actualLinkCaption = categoryPage.getTextLastChainNavigateLink();
+        String actualLinkCaption = categoryPage.getLastChainNavigateLinkText();
         Assert.assertEquals(actualLinkCaption, "Electronics");
     }
+
 }
